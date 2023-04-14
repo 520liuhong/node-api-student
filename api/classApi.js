1446bb45cdcff8869ec50442364d4c17921e8af4
刘洪洪 <2498601771@qq.com>
2023-04-14 18:28:40 +0800

    fix: 抽象接口公共方法

--- api/classApi.js
+++ api/classApi.js
@@ -3,13 +3,27 @@ const {callBackSuc, callBackError, pagination} = require('../utils/utils.js')
 const {classSQL} = require('../db/classSql')
 const code = -1
 
+const basePost = (res, sql, params) => {
+    let _data;
+    pool.getConnection((err, conn) => {
+        conn.query(sql, params, (e, result) => {
+            if (e) _data = callBackError(code, e)
+            if (result) {
+                _data = callBackSuc('删除成功')
+            } else {
+                _data = callBackError(code, '删除失败')
+            }
+            resJson(res, _data)
+        })
+        pool.releaseConnection(conn) // 释放连接池，等待别的连接使用
+    })
+}
 /**
  * 分页查询所有班级
  */
 router.post('/getClass', (req, res) => {
     let _data;
     let limit = pagination(req.body.pageNo, req.body.pageSize)
-    console.log(classSQL.getClass)
     pool.getConnection((err, conn) => {
         conn.query(classSQL.getClass + limit, (e, result) => {
             conn.query(classSQL.getClassTotal, (e1, result1) => {
@@ -24,6 +38,13 @@ router.post('/getClass', (req, res) => {
         })
         pool.releaseConnection(conn) // 释放连接池，等待别的连接使用
     })
+})
+/**
+ * 删除班级
+ */
+router.post('/delClass', (req, res) => {
+    let param = [req.body.ids]
+    basePost(res, classSQL.delClass, param)
 })
 
 module.exports = router;
--- db/classSql.js
+++ db/classSql.js
@@ -8,6 +8,6 @@
 exports.classSQL = {
     // 获取年级接口
     getClass: 'select na_class.Id as id,na_class.class_id as classId,na_class.class_name as className,na_class.college_id as collegeId,na_college.college_name as collegeName,na_specialty.specialty_id as specialtyId,na_specialty.specialty_name as specialtyName,na_teacher.teacher_name as teacher from na_class, na_college, na_specialty, na_teacher where na_class.college_id=na_college.college_id and na_class.specialty_id = na_specialty.specialty_id and na_class.head_teacher = na_teacher.teacher_id',
-    getClass1: 'select * from na_class',
-    getClassTotal: 'select count(Id) from na_class'
+    getClassTotal: 'select count(Id) from na_class',
+    delClass: 'delete from na_class where Id in (?)'
 }
