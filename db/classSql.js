67c47405cffa10a53254e362abe7b0b28f6514ff
刘洪洪 <2498601771@qq.com>
2023-04-19 18:10:19 +0800

    feat: 完善获取班级接口排序和命名，并补充年级字段

--- db/classSql.js
+++ db/classSql.js
@@ -7,7 +7,7 @@
 
 exports.classSQL = {
     // 获取年级接口
-    getClass: 'select na_class.Id as id,na_class.class_id as classId,na_class.class_name as className,na_class.college_id as collegeId,na_college.college_name as collegeName,na_specialty.specialty_id as specialtyId,na_specialty.specialty_name as specialtyName,na_teacher.teacher_name as teacher from na_class, na_college, na_specialty, na_teacher where na_class.college_id=na_college.college_id and na_class.specialty_id = na_specialty.specialty_id and na_class.head_teacher = na_teacher.teacher_id',
+    getClass: 'select na_class.Id as id,na_class.grade_id as gradeId,na_class.class_id as classId,na_class.class_name as class,na_grade.grade_name as grade,na_class.college_id as collegeId,na_college.college_name as college,na_specialty.specialty_id as specialtyId,na_specialty.specialty_name as specialty,na_teacher.teacher_id as teacherId,na_teacher.teacher_name as teacher from na_class, na_college, na_specialty,na_grade, na_teacher where na_class.college_id=na_college.college_id and na_class.grade_id=na_grade.grade_id and na_class.specialty_id = na_specialty.specialty_id and na_class.head_teacher = na_teacher.teacher_id order by na_class.class_id desc',
     getClassTotal: 'select count(Id) from na_class',
     delClass: 'delete from na_class where Id in (?)'
 }
